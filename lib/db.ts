7f8310c5e2d85143cbe258819d7035fd14b8820e
nathan kim <nathanckim18@gmail.com>
2024-06-26 15:56:05 -0400

    limit

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 
 const db = new Database("db.sqlite", { verbose: console.log });
 
-db.prepare(
+db.exec(
   `CREATE TABLE IF NOT EXISTS cards (
 id INTEGER PRIMARY KEY,
 creationTime INTEGER,
@@ -10,7 +10,20 @@ title STRING,
 content STRING,
 x REAL,
 y REAL
-)`
-).run();
+  );`)
+
+db.exec(`DROP TRIGGER IF EXISTS row_limit_trigger;`)
+
+db.exec(
+`CREATE TRIGGER row_limit_trigger
+AFTER INSERT ON cards
+BEGIN
+    SELECT CASE
+        WHEN (SELECT COUNT(*) FROM cards) > 10 THEN
+            -- If the row count exceeds the limit, raise an exception
+            RAISE(FAIL, 'Row limit exceeded')
+    END;
+END;`
+)
 
 export default db;
