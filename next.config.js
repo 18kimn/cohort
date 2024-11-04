4b8c2b571316fcd9a28bff0f76ca61704ecd6740
nathan kim <nathanckim18@gmail.com>
2024-11-03 19:21:06 -0500

    redirect

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/fishes",
+        destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        permanent: true,
+      },
+    ];
+  },
+};
