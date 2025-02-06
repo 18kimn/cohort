d837675a04d9a29e5f7bd859966ba5d16664a97a
nathan kim <nathanckim18@gmail.com>
2025-02-06 00:22:27 -0500

    poem

---
+++ pages/poem/index.tsx
@@ -0,0 +1,17 @@
+export default function Poem() {
+  return (
+    <div>
+      <p>
+        we were living free
+        <br />
+        like the bag
+        <br />
+        the guy was asked me
+        <br />
+        boss you like cheeseburgers
+        <br />
+        and then he gave us a free cheeseburger
+      </p>
+    </div>
+  );
+}
